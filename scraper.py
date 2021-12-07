3bdc92425d5caf261d14cb2141c3b9759d2e163d
dan-bouchard <dan.bouchard96@gmail.com>
2021-12-07 21:12:44 +0000

    Got webdriver working without locking out

--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
+import undetected_chromedriver.v2 as uc
+#import random
+
 
 class WebDriver():
     '''
@@ -27,8 +30,24 @@ class WebDriver():
         # TODO: use a with block to open connection and close when finished
         chrome_options = Options()
         chrome_options.add_experimental_option("detach", True)
+        # chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+        # chrome_options.add_argument('proxy-server=106.122.8.54:3128')
+        chrome_options = Options()
+        chrome_options.add_argument("--start-maximized")
+        chrome_options.add_argument("--disable-infobars")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--disable-popup-blocking")
+        chrome_options.add_experimental_option("useAutomationExtension", False)
+        chrome_options.add_experimental_option("excludeSwitches", ['enable-automation']) 
+        #options = uc.ChromeOptions()
+        #options.headless=False
+        #self.driver = uc.Chrome(version_main=96)
+        #self.driver = uc.Chrome(options=options) 
+        
+        #chrome = uc.Chrome(options=options) 
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get(self.url)
+        # sleep(1)
         self.accept_all_cookies()
         
     
@@ -39,14 +58,18 @@ class WebDriver():
         Returns:
             None
         '''
-        sleep(1) # Ensure the webpage has fully loaded first
+        # sleep(2*random.random()) # Ensure the webpage has fully loaded first
+        sleep(1)
         accept_cookies_button = self.driver.find_element_by_xpath('//*[@id="onetrust-accept-btn-handler"]')
         print(type(accept_cookies_button))
         accept_cookies_button.click()
+        sleep(10)
     
-    def navigate_to_menu_bar(self):
+    # Navigate straight to Men's or Women's clothes page
+
+    def navigate_to_gender(self):
         '''
-        This function navigates to the menu bar container and returns the WebElement
+        This function navigates to the gender page and returns the WebElement
         '''
         # TODO: Navigate via xpath to the menu bar container
         pass
@@ -127,18 +150,26 @@ class StoreData():
     def upload_image_to_datalake(self):
         pass
 
+# 500 items of Mens & 500 of Womens
+number_of_items = {'Mens': {'Hoodies & Sweatshirts' : 50, 'Coats & Jackets': 50, 'Jeans' : 50,'Jewellry & Watches' : 50, 
+                            'Shoes' : 50, 'T-Shirts' : 50, 'Accessories' : 50, 'Trousers' : 50, 'Shirts' : 50, 'Loungewear' : 50},  
+                    'Womens': {'Hoodies & Sweatshirts' : 50, 'Coats & Jackets' : 50, 'Jeans' : 50, 'Jewellry & Watches' : 50,
+                            'Shoes' : 50, 'Tops' : 50, 'Accessories' : 50, 'Bottoms' : 50, 'Dresses & Jumpsuits' : 50,'Loungewear' : 50}}
 
 # List of womens items categories to navigate to, input to a function
-womens_items = ['Tops', 'Hoodies & Sweatshirts', 'Dresses & Jumpsuits', 'Coats & Jackets', 'Knitwear'
-                'Bottoms', 'Jeans', 'Lingerie', 'Loungewear', 'Jewellry & Watches', 'Accessories', 'Shoes']
+#womens_items = ['Tops', 'Hoodies & Sweatshirts', 'Dresses & Jumpsuits', 'Coats & Jackets', 'Knitwear'
+#                'Bottoms', 'Jeans', 'Lingerie', 'Loungewear', 'Jewellry & Watches', 'Accessories', 'Shoes']
 
 # List of mens items categories to navigate to, input to a function
-mens_items = ['Hoodies & Sweatshirts', 'T-Shirts', 'Shirts', 'Jumpers & Knitwear', 'Coats & Jackets', 'Trousers'
-                'Jeans', 'Joggers and Track Pants', 'Shorts and Swim', 'Jewellry & Watches',' Hats & Caps', 
-                'Socks', 'Loungewear & Underwear', 'Accessories', 'Shoes']
+# mens_items = ['Hoodies & Sweatshirts', 'T-Shirts', 'Shirts', 'Jumpers & Knitwear', 'Coats & Jackets', 'Trousers'
+#                 'Jeans', 'Joggers and Track Pants', 'Shorts and Swim', 'Jewellry & Watches',' Hats & Caps', 
+#                 'Socks', 'Loungewear & Underwear', 'Accessories', 'Shoes']
 
 # List of features to scrape from each item, input to a function
 feature = ['Price', 'Sizes', 'Colour', 'Brand', 'Discounted', 'Review Score', 'Number of Reviews']
+
+# TODO: Create a pandas data frame, with each feature, and URL, and unique ID
+
 
 def run_scraper():
     URL = "https://www.urbanoutfitters.com/en-gb/"
