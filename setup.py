75b1da9f36d90ee6e624e3648c79aa133f79bac6
dan-bouchard <dan.bouchard96@gmail.com>
2022-01-25 20:43:01 +0000

    Added a setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = 'h_and_m_scraper',
+    version = '0.0.1',
+    description = 'A scraper of the H&M website for both mens and womens clothes...',
+    url = 'https://github.com/IvanYingX/Urbanoutfitters-Scraper-Project',
+    author = 'James Overend, Quratulaen Ikram, Dan Bouchard',
+    license = 'MIT',
+    packages = find_packages(),
+    install_requires = ['boto3', 'selenium', 'sklearn', 'tqdm'],
+)
